4d6d29d3573be58c9c989e9f09b7e7b8b9cc15ba
Sonu Tiwari <ankorha@gmail.com>
2018-04-10 18:27:58 +0530

    fixing CTRL + C CTRL + V error

--- training/js/__tests__/subtract.test.js
+++ training/js/__tests__/subtract.test.js
@@ -1,7 +1,7 @@
 /*
 Writing test case for subtract 2 numbers.
 */
-const multiply = require('../subtract');
+const subtract = require('../subtract');
 
 test('subtracts 2 - 1 to equal 1', () => {
   expect(subtract(2, 1)).toBe(1);
