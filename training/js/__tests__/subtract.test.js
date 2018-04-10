d58339c79e265fdcc93a088ebf9ea171f0a60c97
Sonu Tiwari <ankorha@gmail.com>
2018-04-10 18:16:32 +0530

    fixing typo

--- training/js/__tests__/subtract.test.js
+++ training/js/__tests__/subtract.test.js
@@ -1,7 +1,7 @@
 /*
 Writing test case for subtract 2 numbers.
 */
-const multiply = require('../subtract');
+const multiply = require('../substract');
 
 test('subtract 2 - 1 to equal 1', () => {
   expect(subtract(2, 1)).toBe(1);
